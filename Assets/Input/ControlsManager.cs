1d34cdf52dc981a02d27856700d8797be822bd17
WackoJack10 <jackanthony14@hotmail.com>
2023-02-04 13:20:39 +0000

    sprint v2

--- Assets/Input/ControlsManager.cs
+++ Assets/Input/ControlsManager.cs
@@ -21,8 +21,4 @@ public class ControlsManager : MonoBehaviour
     {
         return controls.Gameplay.Look.ReadValue<Vector2>();
     }
-    public bool GetIsSprinting()
-    {
-        return controls.Gameplay.Sprint.ReadValue<float>()>0.5f;
-    }
 }
