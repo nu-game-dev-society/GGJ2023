59b9646b8f751bb0dd3fcc0458c7454c7f0c239b
Andrew Nattress <andrew@boxmodeldigital.com>
2023-02-04 13:04:26 +0000

    feature: weapon anims coded

--- Assets/Scripts/Weapons/WeedWacker.cs
+++ Assets/Scripts/Weapons/WeedWacker.cs
@@ -1,32 +1,58 @@
-using JetBrains.Annotations;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WeedWacker : Weapon
 {
-    private InputAction FireAction;
+    [SerializeField]
+    bool firing;
+    [SerializeField]
+    private Vector3 attackPos, startPos;
+    [SerializeField]
+    private Transform weaponModel;
     private void Start()
     {
+        currentFuel = TotalFuel;
         GameManager.Instance.Controls.controls.Gameplay.Fire.performed += Fire;
+        GameManager.Instance.Controls.controls.Gameplay.Fire.canceled += Fire;
     }
-
-
     public void Fire(InputAction.CallbackContext ctx)
     {
-        if(ctx.phase == InputActionPhase.Performed)
+        if (ctx.phase == InputActionPhase.Performed)
         {
             Fire();
+        }
+        if (ctx.phase == InputActionPhase.Canceled)
+        {
+            EndFire();
         }
     }
     public override void Fire()
     {
-        Debug.Log("Fire");
+        firing = true;
+    }
+    private void EndFire()
+    {
+        firing = false;
     }
     public override void Reload()
     {
+        currentFuel = TotalFuel;
     }
-
+    private bool Firing()
+    {
+        return firing;
+    }
+    private void Update()
+    {
+        if (Firing())
+        {
+            weaponModel.transform.localPosition = Vector3.Lerp(weaponModel.transform.localPosition, attackPos, Time.deltaTime * 6.0f);
+            currentFuel -= Time.deltaTime * 5;
+        }
+        else
+        {
+            weaponModel.transform.localPosition = Vector3.Lerp(weaponModel.transform.localPosition, startPos, Time.deltaTime * 2.0f);
+            currentFuel -= Time.deltaTime;
+        }
+    }
 }
